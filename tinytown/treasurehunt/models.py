69b9025fb659ad5b8df178bf88c04d8dfd9438d7
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2022-10-01 14:37:06 +0100

    More models

--- tinytown/treasurehunt/models.py
+++ tinytown/treasurehunt/models.py
@@ -8,7 +8,9 @@ class Code(Model):
     Order = PositiveIntegerField(null=True,blank=True)
     AnswerDigit = PositiveIntegerField(null=True, blank=True)
     AnswerLocation = CharField(max_length=100, blank=True)
-    Location = CharField(max_length=100, blank=True)
+    LocationDescription = CharField(max_length=100, blank=True)
+    Latitude = FloatField()
+    Longitude = FloatField()
     Hint = CharField(max_length=250)
     HintType = SmallIntegerField(choices=HintTypeChoices.choices,default=1)
     def __str__(self):
