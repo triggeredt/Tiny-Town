ef4c3e8a67ca21dce5dde177b81fdf25033f62c4
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2022-10-01 14:15:02 +0100

    Models

--- tinytown/treasurehunt/models.py
+++ tinytown/treasurehunt/models.py
@@ -2,13 +2,20 @@ from django.db.models import *
 from django.conf import settings
 
 class Code(Model):
-    Order = PositiveIntegerField()
+    class HintTypeChoices(IntegerChoices):
+        POINTER = 1
+        DIGIT = 2
+    Order = PositiveIntegerField(null=True)
     Answer = PositiveIntegerField(null=True)
-    Latitude = FloatField()
-    Longitude = FloatField()
-    Hint = CharField(max_length=100)
+    Location = CharField(max_length=100, blank=True)
+    Hint = CharField(max_length=250)
+    HintType = SmallIntegerField(choices=HintTypeChoices.choices,default=1)
+    def __str__(self):
+        return '%i. %s' % (self.Order, self.Hint)
 
 class CodeFind(Model):
     User = ForeignKey(settings.AUTH_USER_MODEL, on_delete=CASCADE)
     Code = ForeignKey(Code, on_delete=CASCADE)
     Answer = PositiveIntegerField(null=True)
+    def __str__(self):
+        return '%s: %s' % (str(self.User), str(self.Code))
--- tinytown/treasurehunt/views.py
+++ tinytown/treasurehunt/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from .models import *
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
+
 class HomeView(LoginRequiredMixin, View):
     def get(self, request):
         return render(request, 'treasurehunt/home_placeholder.html')
+
+class CodeView(LoginRequiredMixin, View):
+    def get(self, request, code_id):
+        code = get_object_or_404(Code, pk=code_id)
+        try:
+            user_answer = CodeFind.objects.get(User=request.user, Code=code).Answer
+        except CodeFind.DoesNotExist:
+            user_answer = ''
+        return render(request, 'treasurehunt/code_placeholder.html', {'code':code, 'user_answer':user_answer})
+    def post(self, request, code_id):
+        if request.POST['answer'].isnumeric():
+            codefind, created = CodeFind.objects.update_or_create(User=request.user, Code_id=code_id, defaults={'Answer':int(request.POST['answer'])})
+            return HttpResponse("Submitted" if created else "Updated")
+        else:
+            return HttpResponseBadRequest()
+
+class CheckInView(LoginRequiredMixin, View):
+    def get(self, request):
+        return render(request, 'treasurehunt/checkin_placeholder.html')
+    def post(self, request):
+        return HttpResponseRedirect()
