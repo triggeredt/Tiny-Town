8e9d71af14c99262afbfd39e2788978bc1e5d078
Joe Greaves <74414406+Grvs44@users.noreply.github.com>
2022-10-01 17:38:33 +0100

    API Pagination

--- tinytown/tinytown/settings.py
+++ tinytown/tinytown/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,12 @@ STATIC_ROOT = BASE_DIR / 'static'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
+
+REST_KNOX = {
+    'TOKEN_TTL': timedelta(hours=20),
+}
